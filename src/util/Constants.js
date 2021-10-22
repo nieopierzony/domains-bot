b1dc8405eb685b091d0ace46c20f3a30d3ccffaa
Philipp Zelinski <43653566+nieopierzony@users.noreply.github.com>
2021-10-23 02:15:45 +0300

    feat(google): added Google.isSafe

---
+++ src/util/Constants.js
@@ -0,0 +1,4 @@
+'use strict';
+
+exports.SAFEBROWSING_ENDPOINT =
+  'https://transparencyreport.google.com/transparencyreport/api/v3/safebrowsing/status?site=';
---
+++ src/util/Google.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const fetch = require('node-fetch');
+const { log } = require('winston');
+
+const { SAFEBROWSING_ENDPOINT } = require('./Constants');
+const Util = require('./Util');
+
+module.exports = class Google extends null {
+  /**
+   * Looks for a domain in Google's registry of restricted sites
+   * @param {string} domain Domain or URL of needed site
+   * @returns {Promise<Boolean>} Whether the domain is in the google registryl
+   */
+  static async isSafe(domain) {
+    // Validate domain
+    // If it's like URL (https://smth.com/test), get needed domain with Util
+    if (Util.isURL(domain)) return this.isSafe(Util.domainFromURL(domain));
+    if (!Util.isDomain(domain)) throw new TypeError('Provided domain is invalid');
+
+    // Request google API and process response
+    const req = await fetch(`${SAFEBROWSING_ENDPOINT}${domain}`);
+    const res = await req.text();
+    const parsed = JSON.parse(res.substring(6));
+
+    const isSafe = parsed[0][1] === 1 || parsed[0][1] === 6;
+    return isSafe;
+  }
+};

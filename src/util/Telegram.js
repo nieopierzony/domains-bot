788e1767cd00d64413b051ad0871cd100dae3351
Philipp Zelinski <43653566+nieopierzony@users.noreply.github.com>
2021-10-24 12:59:00 +0300

    fix(Telegram.send): return msg data on send

--- src/util/Telegram.js
+++ src/util/Telegram.js
@@ -18,14 +18,16 @@ module.exports = class Telegram extends null {
       const hasPhoto = ctx.update?.callback_query?.message?.photo;
       if (ctx.message || hasPhoto) {
         if (hasPhoto) await ctx.editMessageReplyMarkup();
-        await ctx.replyWithHTML(content, markup);
+        const msg = await ctx.replyWithHTML(content, markup);
+        return msg;
       } else {
         await ctx.answerCbQuery();
         await ctx.editMessageText(content, markup);
       }
     } catch (err) {
-      if (err.message.includes('message is not modified')) return;
+      if (err.message.includes('message is not modified')) return false;
       console.error(err);
     }
+    return true;
   }
 };
